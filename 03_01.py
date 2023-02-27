7eab960caa8602b9d39de546f6805de2f6f7e975
h4Treton <h4Treton@gmail.com>
2023-02-27 13:26:05 +0300

    task1

--- 03_01.py
+++ 03_01.py
@@ -1,7 +1,7 @@
 def dec_to_bin(dec_number: int):
     binary_str = ''
     max_bite = 0
-    while 2 ** max_bite < dec_number:
+    while 2 ** max_bite <= dec_number:
         max_bite += 1
     for i in range(max_bite - 1, -1, -1):
         if 2 ** i > dec_number:
@@ -9,7 +9,9 @@ def dec_to_bin(dec_number: int):
         else:
             binary_str += '1'
             dec_number -= 2 ** i
+    if binary_str == '':
+        binary_str = '0'
     return binary_str
 
 
-print(dec_to_bin(4897))
+print(dec_to_bin(2))
