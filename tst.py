73be50e8bdf462d50f125df0cbd39869427bec99
h4Treton <h4Treton@gmail.com>
2023-02-27 13:34:45 +0300

    task1

--- tst.py
+++ tst.py
@@ -4,5 +4,5 @@ for x in range(n):
     name = input(f"Input name {x+1}: ")
     email = input(f"Input email {x}: ")
     list_dict[name] = email
-for name,email in list_dict.items():
+for name, email in list_dict.items():
     print(f"{name}--- {email}")
